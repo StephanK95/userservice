7bef601be15344c0168bfa232f5907dfeb64bc6a
Stephan95 <sklamann@googlemail.com>
2021-12-21 08:37:41 +0100

    Initial commit

---
+++ src/main/java/com/klamann/userservice/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.klamann.userservice.repo;
+
+import com.klamann.userservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
